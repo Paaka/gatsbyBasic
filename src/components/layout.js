1e816cd818815b9503fffbf085456cba0d0278fe
Paaka <32105930+Paaka@users.noreply.github.com>
2020-12-06 23:02:10 +0100

    Fixed emotion problem

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import {Global, css} from '@emotion/core';
+// import {Global, css} from '@emotion/core';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    box-sizing:border-box;
+    margin:0;
+
+    margin:0;
+    color: #333;
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI',
+     Roboto, Helvetica, Arial, sans-serif;
+    font-size:18px;
+    line-height: 1.4;
+
+    & + * {
+        margin-top:1rem;
+    }
+`
 
 const Layout = ({ children }) => (
     <>
-        <Global styles={css` `} />
+        <Wrapper>
+        
+        <header></header>
+        <main>{children}</main>
+        </Wrapper>
+       
     </>
-)
+);
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-    <>
+    <Layout>
         <h1>Home</h1>
         <p>Hello Vancouver</p>
         <Link to="/about">&rarr; Find out more</Link>
-    </>
+    </Layout>
 );
